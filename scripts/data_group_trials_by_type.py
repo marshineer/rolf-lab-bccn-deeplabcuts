a3226016736795a860942594f9246b7836be48b2
Marshall Mykietyshyn <mmykietyshyn@gmail.com>
2024-02-14 01:00:47 +0100

    Fixed typing and references. Fixed path errors.

--- scripts/data_group_trials_by_type.py
+++ scripts/data_group_trials_by_type.py
@@ -2,14 +2,14 @@ import os
 import sys
 import pickle
 import argparse
-from typing import Any
 
 sys.path.insert(0, os.path.abspath(".."))
 from utils.data_loading import get_files_containing
 from data_extract_individual_trials import load_session_trials, TrialData
 
 
-def load_grouped_trials(filepath: str, overwrite: bool) -> dict[str, Any]:
+# dict[str, list[str] | dict[str, list[TrialData]]]
+def load_grouped_trials(participant_id: str, overwrite: bool = False) -> dict[str, list | dict[str, list[TrialData]]]:
     """Loads the participant trial data, which is grouped across experiment conditions and sessions.
 
     Parameters
@@ -20,8 +20,9 @@ def load_grouped_trials(filepath: str, overwrite: bool) -> dict[str, Any]:
         (dict): TrialData instances for all existing sessions, grouped by experiment and change type
     """
 
-    if not overwrite and os.path.exists(filepath):
-        with open(filepath, "rb") as f:
+    grouped_fpath = f"../data/pipeline_data/{participant_id}/{participant_id}_grouped_trial_data.pkl"
+    if not overwrite and os.path.exists(grouped_fpath):
+        with open(grouped_fpath, "rb") as f:
             return pickle.load(f)
     else:
         return {
@@ -43,7 +44,7 @@ def load_grouped_trials(filepath: str, overwrite: bool) -> dict[str, Any]:
 
 def group_participant_trials(
         session_trials: list[list[TrialData]],
-        grouped_trials: dict[str, Any],
+        grouped_trials: dict[str, list | dict[str, list[TrialData]]],
         session_id: str,
 ) -> None:
     """Group the trials from a session by the experiment type.
@@ -70,14 +71,13 @@ def group_participant_trials(
 
 
 def main(overwrite: bool):
-    fpaths, files = get_files_containing("../data/pipeline_data", "trial_data.pkl")
+    fpaths, files = get_files_containing("../data/pipeline_data", "trial_data.pkl", "grouped")
     participant_list = list(set([fpath.split("/")[-2] for fpath in fpaths]))
     participant_list.sort()
     for participant_id in participant_list:
         # Load or initialize the grouped trial dataclass
         grouped_fpath = f"../data/pipeline_data/{participant_id}/"
-        grouped_fname = f"{participant_id}_grouped_trial_data.csv"
-        grouped_trials = load_grouped_trials(os.path.join(grouped_fpath, grouped_fname), overwrite)
+        grouped_trials = load_grouped_trials(participant_id, overwrite)
 
         session_paths, session_files = get_files_containing(grouped_fpath, "trial_data.pkl", "grouped")
         for fpath, file in zip(session_paths, session_files):
@@ -94,7 +94,7 @@ def main(overwrite: bool):
             group_participant_trials(session_trials, grouped_trials, session_id)
 
         # Save the transformed hand data
-        with open(os.path.join(grouped_fpath, grouped_fname), "wb") as f:
+        with open(os.path.join(grouped_fpath, f"{participant_id}_grouped_trial_data.pkl"), "wb") as f:
             pickle.dump(grouped_trials, f)
 
 
